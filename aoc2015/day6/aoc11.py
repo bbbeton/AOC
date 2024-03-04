3840d32412c623e01518d283ddb7f5ebc3255876
bbbeton <jasiu.bloniarz@gmail.com>
2024-03-04 11:41:08 +0100

    start aoc11

---
+++ aoc2015/day6/aoc11.py
@@ -0,0 +1,12 @@
+with open('aoc11-12.txt') as file_input:
+    file = file_input.read().splitlines()
+
+def read_commend(char):
+    i = 0
+    commend = ''
+    while not char[i].isdigit():
+        commend += char[i]
+        i += 1
+    return(commend)
+
+print(read_commend(file[0]))
