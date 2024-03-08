12521829880211508484e2d4043647c80d6143a9
bbbeton <jasiu.bloniarz@gmail.com>
2024-03-08 16:34:15 +0100

    Correct solutions for day6

--- aoc2015/day6/aoc11.py
+++ aoc2015/day6/aoc11.py
@@ -1,13 +1,13 @@
 with open('aoc11-12.txt') as file_input:
     file = file_input.read().splitlines()
 
-def read_commend(string):
+def read_command(string):
     i = 0
-    commend = ''
+    command = ''
     while not string[i].isdigit():
-        commend += string[i]
+        command += string[i]
         i += 1
-    return(commend.strip())
+    return(command.strip())
 
 #print(read_commend(file[0]))
 
@@ -38,62 +38,21 @@ def turn_off(light):
 
 if __name__ == "__main__":
     lights = create_table()
-    for k in range (len(file)):
-        commend = read_commend(file[k])
-        print(commend)
+    for k in range(len(file)):
+        command = read_command(file[k])
         curr_lights = get_nums(file[k])
-        print(curr_lights)
-        if commend == 'toggle':
-            if int(curr_lights[0][0]) != int(curr_lights[1][0]) and int(curr_lights[0][1]) != int(curr_lights[1][1]):
-                for i in range(int(curr_lights[0][0]),int(curr_lights[1][0]) + 1):
-                    for j in range(int(curr_lights[0][1]),int(curr_lights[1][1]) + 1):
-                        lights[i][j] = toggle(lights[i][j])
-            elif int(curr_lights[0][0]) == int(curr_lights[0][1]) and int(curr_lights[1][0]) != int(curr_lights[1][1]):
-                i = curr_lights[0][0]
-                for j in range(int(curr_lights[1][0]),int(curr_lights[1][1]) + 1):
+        if command == 'toggle':
+            for i in range(int(curr_lights[0][0]), int(curr_lights[1][0]) + 1):
+                for j in range(int(curr_lights[0][1]), int(curr_lights[1][1]) + 1):
                     lights[i][j] = toggle(lights[i][j])
-            elif int(curr_lights[0][0]) != int(curr_lights[0][1]) and int(curr_lights[1][0]) == int(curr_lights[1][1]):
-                for i in range(int(curr_lights[0][0]),int(curr_lights[0][1]) + 1):
-                    j = curr_lights[1][0]
-                    lights[i][j] = toggle(lights[i][j])
-            elif int(curr_lights[0][0]) == int(curr_lights[0][1]) and int(curr_lights[1][0]) == int(curr_lights[1][1]):
-                j = int(curr_lights[1][0])
-                i = int(curr_lights[0][0])
-                lights[i][j] = toggle(lights[i][j])
-        elif commend == 'turn on':
-            if int(curr_lights[0][0]) != int(curr_lights[1][0]) and int(curr_lights[0][1]) != int(curr_lights[1][1]):
-                for i in range(int(curr_lights[0][0]),int(curr_lights[1][0]) + 1):
-                    for j in range(int(curr_lights[0][1]),int(curr_lights[1][1]) + 1):
-                        lights[i][j] = turn_on(lights[i][j])
-            elif int(curr_lights[0][0]) == int(curr_lights[0][1]) and int(curr_lights[1][0]) != int(curr_lights[1][1]):
-                i = curr_lights[0][0]
-                for j in range(int(curr_lights[0][1]),int(curr_lights[1][1]) + 1):
+        elif command == 'turn on':
+            for i in range(int(curr_lights[0][0]), int(curr_lights[1][0]) + 1):
+                for j in range(int(curr_lights[0][1]), int(curr_lights[1][1]) + 1):
                     lights[i][j] = turn_on(lights[i][j])
-            elif int(curr_lights[0][0]) != int(curr_lights[0][1]) and int(curr_lights[1][0]) == int(curr_lights[1][1]):
-                for i in range(int(curr_lights[0][0]),int(curr_lights[1][0]) + 1):
-                    j = curr_lights[1][0]
-                    lights[i][j] = turn_on(lights[i][j])
-            elif int(curr_lights[0][0]) == int(curr_lights[0][1]) and int(curr_lights[1][0]) == int(curr_lights[1][1]):
-                j = int(curr_lights[1][0])
-                i = int(curr_lights[0][0])
-                lights[i][j] = turn_on(lights[i][j])
-        elif commend == 'turn off':
-            if int(curr_lights[0][0]) != int(curr_lights[1][0]) and int(curr_lights[0][1]) != int(curr_lights[1][1]):
-                for i in range(int(curr_lights[0][0]),int(curr_lights[1][0]) + 1):
-                    for j in range(int(curr_lights[0][1]),int(curr_lights[1][1]) + 1):
-                        lights[i][j] = turn_off(lights[i][j])
-            elif int(curr_lights[0][0]) == int(curr_lights[0][1]) and int(curr_lights[1][0]) != int(curr_lights[1][1]):
-                i = curr_lights[0][0]
-                for j in range(int(curr_lights[0][1]),int(curr_lights[1][1]) + 1):
+        elif command == 'turn off':
+            for i in range(int(curr_lights[0][0]), int(curr_lights[1][0]) + 1):
+                for j in range(int(curr_lights[0][1]), int(curr_lights[1][1]) + 1):
                     lights[i][j] = turn_off(lights[i][j])
-            elif int(curr_lights[0][0]) != int(curr_lights[0][1]) and int(curr_lights[1][0]) == int(curr_lights[1][1]):
-                for i in range(int(curr_lights[0][0]),int(curr_lights[1][0]) + 1):
-                    j = curr_lights[1][0]
-                    lights[i][j] = turn_off(lights[i][j])
-            elif int(curr_lights[0][0]) == int(curr_lights[0][1]) and int(curr_lights[1][0]) == int(curr_lights[1][1]):
-                j = int(curr_lights[1][0])
-                i = int(curr_lights[0][0])
-                lights[i][j] = turn_off(lights[i][j])
 
     total_lights_on = sum(sum(row) for row in lights)
     print(total_lights_on)
---
+++ aoc2015/day6/aoc12.py
@@ -0,0 +1,58 @@
+with open('aoc11-12.txt') as file_input:
+    file = file_input.read().splitlines()
+
+def read_command(string):
+    i = 0
+    command = ''
+    while not string[i].isdigit():
+        command += string[i]
+        i += 1
+    return command.strip()
+
+def create_table():
+    lights = []
+    for i in range(0, 1000):
+        lights.append([])
+        for j in range(0, 1000):
+            lights[i].append(0)
+    return lights
+
+def get_nums(string):
+    nums = []
+    words = string.split()
+    for element in words:
+        if element[0].isdigit():
+            nums.append(element.split(','))
+    return nums
+
+def toggle(light):
+    return light + 2
+
+def turn_on(light):
+    return light + 1
+
+def turn_off(light):
+    if light > 0:
+        return light - 1
+    return 0  
+
+if __name__ == "__main__":
+    lights = create_table()
+    for k in range(len(file)):
+        command = read_command(file[k])
+        curr_lights = get_nums(file[k])
+        if command == 'toggle':
+            for i in range(int(curr_lights[0][0]), int(curr_lights[1][0]) + 1):
+                for j in range(int(curr_lights[0][1]), int(curr_lights[1][1]) + 1):
+                    lights[i][j] = toggle(lights[i][j])
+        elif command == 'turn on':
+            for i in range(int(curr_lights[0][0]), int(curr_lights[1][0]) + 1):
+                for j in range(int(curr_lights[0][1]), int(curr_lights[1][1]) + 1):
+                    lights[i][j] = turn_on(lights[i][j])
+        elif command == 'turn off':
+            for i in range(int(curr_lights[0][0]), int(curr_lights[1][0]) + 1):
+                for j in range(int(curr_lights[0][1]), int(curr_lights[1][1]) + 1):
+                    lights[i][j] = turn_off(lights[i][j])
+
+    total_brightness = sum(sum(row) for row in lights)
+    print(total_brightness)
