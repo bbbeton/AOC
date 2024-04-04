5bf651acc0732a134938af9ac54551f406650970
bbbeton <jasiu.bloniarz@gmail.com>
2024-04-04 21:57:47 +0200

    day 10 solutions

---
+++ aoc2015/day10/aoc19.py
@@ -0,0 +1,22 @@
+def count_numbers(number):
+    output_number = ''
+    numbers_count = 0
+    for i in range(len(number)):
+        numbers_count += 1
+        if i < len(number) - 1:
+            if number[i] != number[i+1]:
+                output_number += str(numbers_count) + number[i]
+                numbers_count = 0
+        else:
+            output_number += str(numbers_count) + number[i]
+    return output_number
+
+if __name__ == "__main__":
+    i = 0
+    number = "1113222113"
+    while i < 50:
+        number = count_numbers(number)
+        i += 1
+    print(len(number))
+        
+
