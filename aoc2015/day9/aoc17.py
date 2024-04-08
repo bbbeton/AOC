6fdfead97c111bbf301a3e24b2f4704907b547d0
bbbeton <jasiu.bloniarz@gmail.com>
2024-04-08 10:40:29 +0200

    Day 9 solutions

--- aoc2015/day9/aoc17.py
+++ aoc2015/day9/aoc17.py
@@ -1,3 +1,5 @@
+from itertools import permutations
+
 with open('aoc17-18.txt') as file_input:
     file = file_input.read().splitlines()
     
@@ -16,41 +18,15 @@ def get_cities(distances):
             cities.append(distance[0][1])
     return cities
 
-def find_length(distances, city, cities):
-    current_city = city
-    visited_cities = []
-    route_length = 0
-    while not len(visited_cities) == len(cities):
-        for distance in distances:
-            if current_city in distance[0] and current_city not in visited_cities:
-                visited_cities.append(current_city)
-                if current_city == distance[0][1]:
-                    current_city = distance[0][0]
-                else:
-                    current_city = distance[0][1]
-                if current_city not in visited_cities:
-                    route_length += distance[1]
-    return(route_length)
+def get_length(route, distances):
+    total_distance = 0
+    for i in range(1, len(route)):
+        for j in range(len(distances)):
+            if route[i] in distances[j][0] and route[i-1] in distances[j][0]:
+                total_distance += int(distances[j][1])
+                break
+    return total_distance
 
-# def find_length(distances, city, cities):
-#     current_city = city
-#     visited_cities = []
-#     route_length = 0
-#     while len(visited_cities) < len(cities):
-#         shortest_route = float('inf')
-#         for distance in distances:
-#             if current_city in distance[0] and current_city not in visited_cities:
-#                 if distance[1] < shortest_route and shortest_route != 0:
-#                     shortest_route = distance[1]
-#                     if current_city == distance[0][1]:
-#                         next_city = distance[0][0]
-#                     else:
-#                         next_city = distance[0][1]
-#         visited_cities.append(current_city)
-#         current_city = next_city
-#         route_length += shortest_route
-                
-#     return(route_length)
 
 if __name__ == '__main__':
     distances = []
@@ -58,6 +34,9 @@ if __name__ == '__main__':
     for line in file:
         distances.append(read_distances(line))
     starting_cities = get_cities(distances)
-    for city in starting_cities:
-        routes.append(find_length(distances, city, starting_cities))
-    print(routes)
+    print(starting_cities)
+    possible_routes = permutations(starting_cities)
+    for route in list(possible_routes):
+        routes.append(get_length(route, distances))
+    print(min(routes))
+    # print(routes)
