6245b4831a28e9e76adc749cc85e4c40e91ac49c
bbbeton <jasiu.bloniarz@gmail.com>
2024-12-09 23:38:42 +0100

    Started on day 5 solution

---
+++ aoc2024/day05/aoc9.py
@@ -0,0 +1,18 @@
+with open("aoc9-10.txt") as file_input:
+    file = file_input.read().splitlines()
+
+def getInstructions(textFile):
+    i = 0
+    instructions = []
+    while textFile[i]:
+        instructions.append(textFile[i].split('|'))
+        i += 1
+    return instructions
+
+def getUpdates(textFile):
+    i = 0
+    while textFile[i]:
+        i += 1
+
+if __name__ == "__main__":
+    print(getInstructions(file))
